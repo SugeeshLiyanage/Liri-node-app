a06e4148ed9e1a8eb92358d036dc8ccd638d16a4
sugeesh <sugeeshdeveloper@gmail.com>
2018-04-07 08:55:02 -0500

    corrected twitter

--- liri.js
+++ liri.js
@@ -82,65 +82,34 @@ switch(liriArgument) {
 
 
 
-    // function myTweets() {
-	// 	var client = new twitter({
-	// 		consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    //         consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    //         access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    //         access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-	// 	});
-	// 	var twitterUsername = process.argv[3];
-	// 	if(!twitterUsername){
-	// 		twitterUsername = "Sugeesh_";
-	// 	}
-	// 	params = {screen_name: twitterUsername};
-	// 	client.get("statuses/user_timeline/", params, function(error, data, response){
-	// 		if (!error) {
-	// 			for(var i = 0; i < data.length; i++) {
-	// 				//console.log(response); // Show the full response in the terminal
-	// 				var twitterResults = 
-	// 				"@" + data[i].user.screen_name + ": " + 
-	// 				data[i].text + "\r\n" + 
-	// 				data[i].created_at + "\r\n" + 
-	// 				"------------------------------ " + i + " ------------------------------" + "\r\n";
-	// 				console.log(twitterResults);
-	// 				log(twitterResults); // calling log function
-	// 			}
-	// 		}  else {
-	// 			console.log("Error :"+ error);
-	// 			return;
-	// 		}
-	// 	});
-
-
-
-
-
-        // 
         // Spotify function, uses the Spotify module to call the Spotify api
-	function spotifySong(songName) {
-		
-		spotify.search({ type: "track", query: songName }, function(error, data) {
-			if(!error){
-				for (var i = 0; i < data.tracks.length; i++) {
-					var songData = data.tracks.items[i];
-					console.log("Artist: " + songData.artists[0].name);
-					console.log("Song: " + songData.name);
-					console.log("Preview URL: " + songData.preview_url);
-					console.log("Album: " + songData.album.name);
-					console.log("---------------------------------");
-					
-					
-						
-						log(songData); // calling log function
+		function spotifyThisSong(songName) {
+			var songName = process.argv[3];
+			if(!songName){
+				songName = "What's my age again";
+			}
+			params = songName;
+			spotify.search({ type: "track", query: params }, function(err, data) {
+				if(!err){
+					var songInfo = data.tracks.items;
+					for (var i = 0; i < 5; i++) {
+						if (songInfo[i] != undefined) {
+							var spotifyResults =
+							"Artist: " + songInfo[i].artists[0].name + "\r\n" +
+							"Song: " + songInfo[i].name + "\r\n" +
+							"Album the song is from: " + songInfo[i].album.name + "\r\n" +
+							"Preview Url: " + songInfo[i].preview_url + "\r\n" + 
+							"------------------------------ " + i + " ------------------------------" + "\r\n";
+							console.log(spotifyResults);
+							log(spotifyResults); // calling log function
+						}
 					}
+				}	else {
+					console.log("Error :"+ err);
+					return;
 				}
-				else {
-				console.log("error occured");
-				}
-		});
-	}
-
+			});
+		};
 
             function doWhatItSays() {
                 fs.readFile('random.txt', "utf8", function(error, data) {
